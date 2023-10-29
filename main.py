ecaa26b8e1406c795e6c85799065e61a25383143
Roman Serafymovych <serafymovychr@gmail.com>
2023-10-29 22:32:11 +0300

    string added

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ a = - 2 + 3j
 b = 4 + 2.1j
 result =  a + b
 print(result)
+length = "2.75"
+width = "1.75"
+area = float(length) * float(width)
+show = f"With width {width} and length {float(length)} of the room, its area is equal to {area}."
+print(show)
