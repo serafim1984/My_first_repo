7a8ed8dc743afc7e8d9ffa6ad3082e30f1dd4e1b
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-05 16:23:26 +0300

    Autocheck

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-print("Hello World!")
-print("Hello Git!")
-a = - 2 + 3j
-b = 4 + 2.1j
-result =  a + b
-print(result)
-length = "2.75"
-width = "1.75"
-area = float(length) * float(width)
-show = f"With width {width} and length {float(length)} of the room, its area is equal to {area}."
-print(show)
+num = int(input("Enter integer (0 for output): "))
+sum = 0
+while num != 0:
+    new = num + 1
+    
+    for j in range(new):
+        sum = sum + j
+        j = j + 1
+        
+    num = int(input("Enter integer (0 for output): "))
+
+print(sum)
