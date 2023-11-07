667c4cd86b4bdbcdc8725fe948c399763319a050
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-07 23:26:52 +0300

    Python lesson 1

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-message = input("Enter a message: ")
-offset = int(input("Enter the offset: "))
-encoded_message = ""
-for ch in message:
-    if ord(ch) >= 65 and ord(ch) <= 90:
-        encoded_message = encoded_message + chr((ord(ch) - ord('A') + offset) % 26 + ord('A'))
-        continue
-    elif ord(ch) >= 97 and ord(ch) <= 122 :
-         encoded_message = encoded_message + chr((ord(ch) - ord('a') + offset) % 26 + ord('a'))
-         continue
-    else:
-        encoded_message = encoded_message + ch
+# ctrl (option) + shift + f -> search
+# ctrl + O -> open file
 
-print(encoded_message)
+
+
+a = 2
+
+print("How fast!!!")
+
+a = 3
+
+
+print("hello world")
+
+
