2e703287346b8f67762809f7acff9b583db50cfe
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-11 22:38:00 +0300

    Lecture_2

---
+++ Lectures.py
@@ -0,0 +1,25 @@
+# alt + shift + up/down (select several lines up or down and allow to operate several lines)
+# symbol. + 2 tabs - output all methods of this class
+import calendar
+
+calendar.isleap(year) # check if year is leap year
+
+calendar.calendar(2023) # output caledar for 2023
+
+dir(calendar) # output all methods for class calendar
+
+age = int(input('What is your age >>>> '))
+
+age = 23
+
+type(age) is int
+
+isinstance(age, int)  # check if age is int
+
+
+
+
+
+
+
+
