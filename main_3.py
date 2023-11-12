dad4a003a1f992522154a35f061612dcded8d26f
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-12 15:03:34 +0300

    Autocheck 3-10

--- main_3.py
+++ main_3.py
@@ -1,14 +1,17 @@
-
-
-def format_string(string, length):
-
-    if len(string) >= length:
-        
-        return string
-
+def factorial(n):
+    
+    if n < 2:
+        return 1  # Базовий випадок
     else:
+        return n * factorial(n - 1)
+    
+        
 
-        return "" * ((length - len(string)) // 2) + string
 
-print(format_string('abaa', 15))
+def number_of_groups(n, k):
+
+    return int(factorial(n)/(factorial(n - k) * factorial(k))) # to represent without .0 due to devision
+
+print(number_of_groups(50, 7))
+
       
