009fe531249e7a34c3c841ad221c80c1fb1f9045
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-14 23:00:15 +0300

    lec_3

--- Lectures.py
+++ Lectures.py
@@ -1,6 +1,7 @@
 # alt + shift + up/down (select several lines up or down and allow to operate several lines)
 # symbol. + 2 tabs - output all methods of this class
-import calendar
+
+'''import calendar
 
 calendar.isleap(year) # check if year is leap year
 
@@ -14,7 +15,55 @@ age = 23
 
 type(age) is int
 
-isinstance(age, int)  # check if age is int
+isinstance(age, int)  # check if age is int'''
+
+# kebab-case, snake_case, camelCase, PascalCase
+
+# Ctrl + left click - shows how function coded inside Python in C
+
+
+
+'''def is_odd(number):
+    if number % 2 == 0:
+        print(number, "is even")'''
+
+
+''' n, o, b = (1, 6, "value") # - unpacking of tuple
+
+print(n)
+
+return a, b, c # - retur of several items as a tuple'''
+
+''' a = 0
+
+def a_ch():
+
+    b = 2
+    
+    a = b
+
+    return a
+
+print(a) ''' # - print 0!!!
+
+
+# - "nonlocal' used when function in functions, for defining variable not local at second folled function hierarchy - to use variable from one level up zone of visibility
+
+# args = tuple
+# kwargs = dict
+
+
+''' def intro (firstname, **kwargs):
+    print(kwargs)
+
+    print("firstname", firstname)
+
+    for key, value in kwargs.items(): # - iterate for key and values
+        print (key, value)
+
+intro(firstname = "Olekcsndr", last_name = "Holodetskyi", age = 18, school = "GoIT") '''
+
+
 
 
 
