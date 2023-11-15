847ef5e16d09c24150b3251a5e5aa4e5db18fc89
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-16 00:53:40 +0300

    4_8

--- main_4.py
+++ main_4.py
@@ -1,47 +1,18 @@
-points = {
-    (0, 1): 2,
-    (0, 2): 3.8,
-    (0, 3): 2.7,
-    (1, 2): 2.5,
-    (1, 3): 4.1,
-    (2, 3): 3.9,
-}
+def game(terra, power):
 
+    power_sum = power
 
-def calculate_distance(coordinates):
+    for list in terra:
 
-    sum = 0
+        for list_power in list:
 
-    j = 0
+            if power_sum >= list_power:
 
-    for i in coordinates:
-
-        j = j + 1
-    
-        if j < len(coordinates):
-
-            if i < coordinates[j]:
-
-                for k, l in points.items():
-
-                    if k == (i, coordinates[j]):
-
-                        sum = sum + l
-                        
-                        break
+                power_sum += list_power
 
             else:
 
-                for k, l in points.items():
+                break
 
-                    if k == (coordinates[j], i):
+    return power_sum
 
-                        sum = sum + l
-                        
-                        break
-
-
-
-    return sum
-
-print(calculate_distance((0, 1, 2, 3)))
