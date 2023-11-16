37d368fb86d4b6cb092ae9c8f06bee8d1dcae95a
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-17 00:38:54 +0300

    4_13

--- main_4.py
+++ main_4.py
@@ -1,51 +1,15 @@
-from random import randint
+def parse_folder(path):
+    files = []
+    folders = []
+    
+    for itm in path.iterdir():
 
+        if itm.is_file():
 
-def get_random_password():
-    result = ""
-    count = 0
-    while count < 8:
-        random_symbol = chr(randint(40, 126))
-        result = result + random_symbol
-        count = count + 1
-    return result
+             files.append(itm.name)
 
+        if itm.is_dir():
 
-def is_valid_password(password):
-    if len(password) != 8:
-        return False
-
-    has_upper = False
-    has_lower = False
-    has_num = False
-
-    for ch in password:
-        if ch.isupper():
-            has_upper = True
-        elif ch.islower():
-            has_lower = True
-        elif ch.isdigit():
-            has_num = True
-
-    return has_upper and has_lower and has_num
-
-
-def get_password():
-
-    cnt = 0
-
-    while cnt < 100:
-
-        password = get_random_password()
-
-        if is_valid_password(password):
-
-            return password
-
-        else:
-
-            cnt += 1
-
-            continue
-
-    return
+             folders.append(itm.name) 
+            
+    return files, folders
