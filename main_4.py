05f5c1c78aef756fd96cfb8134eff5d3a2e33bce
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-17 00:07:46 +0300

    4_11

--- main_4.py
+++ main_4.py
@@ -1,12 +1,26 @@
-from random import randint
+def is_valid_password(password):
 
-
-def get_random_password():
-
-    pswr = ""
-
-    for i in range(8):
+    check_1 = False
+    check_2 = False
+    check_3 = False
     
-        pswr = pswr + chr(randint(40,126))
 
-    return pswr
+    if len(password) < 8:
+
+        return False
+
+    for sym in password:
+
+        if ord(sym) > 65 and ord(sym) < 90:
+
+              check_1 = True
+
+        if ord(sym) > 97 and ord(sym) < 122:
+
+              check_2 = True
+
+        if ord(sym) > 48 and ord(sym) < 57:
+
+              check_3 = True
+
+    return check_1 and check_2 and check_3
