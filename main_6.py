0d04574829b9d94406897fdb7e2b065be20e3f21
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-19 19:07:53 +0300

    3

--- main_6.py
+++ main_6.py
@@ -1,66 +1,10 @@
-articles_dict = [
-    {
-        "title": "Endless ocean waters.",
-        "author": "Jhon Stark",
-        "year": 2019,
-    },
-    {
-        "title": "Oceans of other planets are full of silver",
-        "author": "Artur Clark",
-        "year": 2020,
-    },
-    {
-        "title": "An ocean that cannot be crossed.",
-        "author": "Silver Name",
-        "year": 2021,
-    },
-    {
-        "title": "The ocean that you love.",
-        "author": "Golden Gun",
-        "year": 2021,
-    },
-]
+def sanitize_phone_number(phone):
+        
+    phone = phone.strip()
+    phone = phone.replace("+", "")
+    phone = phone.replace(")", "")
+    phone = phone.replace("(", "")
+    phone = phone.replace("-", "")
+    phone = phone.replace(" ", "")
 
-def find_articles(key, letter_case=False):
-    
-    articles = []
-      
-    for art in articles_dict:
-
-        for k, v in art.items():
-
-            if type(v) != str:
-
-                continue
-
-            if letter_case == False:
-
-                key_1 = key.lower()
-                v_1 = v.lower()
-
-                if v_1.find(key_1) != -1:
-
-                    articles.append(art)
-
-                    break
-
-                else: 
-
-                    continue
-
-            else:
-
-                if v.find(key) != -1:
-
-                    articles.append(art)
-
-                    break
-
-                else: 
-
-                    continue
-                
-
-    return articles
-
-print(find_articles("Ocean"))
+    return phone
