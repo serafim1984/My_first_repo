1786e823b3c96e81f7cbc698aba47c5200c957a7
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-21 23:50:05 +0300

    14

--- main_6.py
+++ main_6.py
@@ -1,8 +1,8 @@
 import re
 
 
-def find_all_emails(text):
-    result = re.findall(r"[a-zA-Z]{1}[\w\.]+@+\S{1,4}\.\S{2,3}", text)
+def find_all_phones(text):
+    
+    result = re.findall(r"\+380\(\d{2}\)\d{3}-\d-\d{3}|\+380\(\d{2}\)\d{3}-\d{2}-\d{2}", text)
+    
     return result
-
-print(find_all_emails('Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org first_last@iana.org first.middle.last@iana.or a@test.com abc111@test.com.net'))
