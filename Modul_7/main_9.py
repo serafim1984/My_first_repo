8d75d27057b95e98e5989f52a8322a2e7b09a84e
Roman Serafymovych <serafymovychr@gmail.com>
2023-11-29 19:23:51 +0300

    Correct but does not work for Autocheck

--- Modul_7/main_9.py
+++ Modul_7/main_9.py
@@ -8,9 +8,9 @@ def all_sub_lists(data):
 
         for i in range(data.index(item), len(data)):
 
-            sublist_list.append(data[data.index(item) : i])
+            sublist_list.append(data[data.index(item) : i + 1])
 
     return sublist_list
 
-print(all_sub_lists([1, 2, 3]))
+print(all_sub_lists([4, 6, 1, 3]))
 
