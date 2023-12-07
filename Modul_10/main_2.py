20eb49ae13aed707d039811b06b346e7ee6b9eef
Roman Serafymovych <serafymovychr@gmail.com>
2023-12-07 23:46:28 +0300

    1

---
+++ Modul_10/main_2.py
@@ -0,0 +1,12 @@
+class Animal:
+
+    def __init__(self, nickname, weight):
+
+        self.nickname = nickname
+        self.weight = weight
+
+    def say(self):
+
+        pass
+    
+animal = Animal('Rudy', 4)
