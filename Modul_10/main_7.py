5648f4e3d6c1a2ff1850ecba5f3b3337ecdeeb7d
Roman Serafymovych <serafymovychr@gmail.com>
2023-12-09 16:38:02 +0300

    1

---
+++ Modul_10/main_7.py
@@ -0,0 +1,43 @@
+class Animal:
+    def __init__(self, nickname, weight):
+        self.nickname = nickname
+        self.weight = weight
+
+    def say(self):
+        pass
+
+    def change_weight(self, weight):
+        self.weight = weight
+
+
+class Owner:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def info(self):
+        return {'name' : self.name, 'age' : self.age, 'address' : self.address}
+
+
+
+
+class Dog(Animal):
+    def __init__(self, nickname, weight, breed, owner):
+        self.breed = breed
+        self.owner = owner
+        
+        super().__init__(nickname, weight)
+
+    def say(self):
+        return "Woof"
+
+    def who_is_owner(self):
+        return owner.info()
+
+        
+owner = Owner('Sherlock', 24, 'London, 221B Baker Street')
+dog = Dog('Barbos', 23, 'labrador', owner)
+
+print(dog.who_is_owner())
+
