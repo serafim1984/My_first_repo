fb4bf7daf6cfaf310d86884a68dc0f63727b7604
Roman Serafymovych <serafymovychr@gmail.com>
2023-12-16 20:51:12 +0300

    1

--- Practice_1.py
+++ Practice_1.py
@@ -26,3 +26,17 @@ try:
 except ValueError:
 
     print("Error while checking")
+
+
+'''
+several messages for error
+
+def input_error(error):
+    def error_handler(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except (ValueError, IndexError):
+                print(error)
+        return inner
+    return error_handler'''
